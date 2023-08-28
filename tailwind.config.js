4b6cb3d586a1d89f07f692ac7967e3325aa23960
Abigail Udoh <125694267+abigailudoh@users.noreply.github.com>
2023-08-28 23:10:10 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      'red': 'hsl(7, 99%, 70%)',
+      'Yellow': 'hsl(51, 100%, 49%)',
+      'cyan': 'hsl(167, 40%, 24%)',
+      'blue': 'hsl(198, 62%, 26%)',
+      'cyanish': 'hsl(168, 34%, 41%)',
+      'Vddblue': 'hsl(212, 27%, 19%)',
+      'Vdgblue': 'hsl(213, 9%, 39%)',
+      'Dgblue': 'hsl(232, 10%, 55%)',
+      'Grayishblue': 'hsl(210, 4%, 67%)',
+      'White': 'hsl(0, 0%, 100%)',
+      'headercyan': 'hsl(198,93%,60%)',
+      'footergreen': 'hsl(164,49%,67%)',
+    },
+    
+    extend: {},
+  
+  },
+  plugins: [],
+}
+
